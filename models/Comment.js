428515fb952d0275665f03fa30edccbc2fc16914
Cortez-Dev <omkarparab21@gmail.com>
2020-11-17 19:07:12 +0530

    Comment fix

--- models/Comment.js
+++ models/Comment.js
@@ -5,12 +5,8 @@ const Reply = require('./Reply');
 const Comment = new Schema({
     article_id: {type: Schema.Types.ObjectId, ref: 'Article'},
     author_id: {type: Schema.Types.ObjectId, ref: 'User'},
-<<<<<<< HEAD
-    comment: {type: String, trim: true},
-=======
-    comment : {type: String,trim: true},
-    name : {type: String,trim: true},
->>>>>>> 21f6d7d054f84cf8fb396279398cb8b2eac4384e
+    comment: {type: String,trim: true},
+    name: {type: String,trim: true},
 }, {timestamps: true});
 
 Comment.pre('remove', function(next) {
